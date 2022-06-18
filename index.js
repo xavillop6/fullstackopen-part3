1634d7294c0109c5e9c09cd846ba1973f2b5c2c9
Xavi <xavillop6@gmail.com>
2022-06-18 18:31:43 +0200

    3.11 phonebook full stack

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
+
 
 app.use(morgan(function (tokens, req, res) {
   return [
