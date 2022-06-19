ab9908977aad6af80f430fb461b2b13acaf5271a
Xavi <xavillop6@gmail.com>
2022-06-19 16:53:57 +0200

    3.15: Phonebook database, step3

--- models/Person.js
+++ models/Person.js
@@ -5,6 +5,14 @@ const personSchema = new Schema({
     number: String
 })
 
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
 const Person = model('Person', personSchema)
 
 module.exports = Person
